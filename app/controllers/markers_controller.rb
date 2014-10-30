2cb05e06d2ff0d107d93706e4cf346c0596cc8dc
Zack Sheppard <z@zack.io>
2014-10-30 13:18:17 -0400

    .json GET request of marker instance responds with custom geojson. also cleaned up duplication in the controller logic

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -5,39 +5,22 @@ class MarkersController < ApplicationController
   # GET /markers.json
   def index
     @markers = Marker.all
-    @geojson = []
-    
-    @markers.each do |m|
-      if m.latitude 
-        @geojson << {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [m.longitude, m.latitude]
-          },
-          properties: {
-            id: m.id,
-            name: m.title,
-            number: m.number,
-            description: m.short_desc,
-            :'marker-color' => '#00607d',
-            :'marker-symbol' => 'circle',
-            :'marker-size' => 'medium'
-          }
-        }
-      end
-    end
 
     respond_to do |format|
       format.html
       format.csv { send_data @markers.to_csv }
-      format.json { render json: @geojson }
+      format.json { render json: @markers.to_json }
     end
   end
 
   # GET /markers/1
   # GET /markers/1.json
   def show
+    @marker = Marker.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @marker.to_json }
+    end
   end
 
   # GET /markers/new
--- app/models/marker.rb
+++ app/models/marker.rb
@@ -122,27 +122,23 @@ class Marker < ActiveRecord::Base
   end
 
   def to_json
-    # @markers = Marker.all
     @geojson = []
-    
-    # @markers.each do |m|
-      @geojson << {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [self.latitude, self.longitude]
-        },
-        properties: {
-          id: self.id,
-          name: self.title,
-          number: self.number,
-          description: self.description,
-          :'marker-color' => '#00607d',
-          :'marker-symbol' => 'circle',
-          :'marker-size' => 'medium'
-        }
+    @geojson << {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [self.latitude, self.longitude]
+      },
+      properties: {
+        id: self.id,
+        name: self.title,
+        number: self.number,
+        description: self.description,
+        :'marker-color' => '#00607d',
+        :'marker-symbol' => 'circle',
+        :'marker-size' => 'medium'
       }
-    # end
+    }
   end
 
   def short_desc
