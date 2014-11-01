f181fb2a0eb0273f4998d9eeea0556a0b9102c50
Zack Sheppard <z@zack.io>
2014-11-01 15:15:45 -0400

    adds note one what dms means

--- app/models/marker.rb
+++ app/models/marker.rb
@@ -60,6 +60,7 @@ class Marker < ActiveRecord::Base
   # Marker.convert_geo("N39° 09' 12.9''")
   # w/o the .0 it was => 39.18733333333333, -119.70636111111111
 
+  # dms is geo location shorthand for Degrees Minutes Seconds.
   def convert_geo(dms)
     dms_array = dms.scan(/[0-9.]+/)
     coordinate = dms_array[0].to_f + dms_array[1].to_f/60.0 + dms_array[2].to_f/3600.0
